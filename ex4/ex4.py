6d338696c23f8b8d2680349c04ebca1cabd7581a
t3min4l <hainam.nguyen.ict@gmail.com>
2017-11-24 10:15:00 +0700

    back prop

--- ex4/ex4.py
+++ ex4/ex4.py
@@ -60,6 +60,7 @@ input_size = 400
 hidden_layer_size = 25
 num_labels = 10
 learning_rate = 1
+J = 0
 
 params = (np.random.random(size=hidden_layer_size * (input_size + 1) + num_labels * (hidden_layer_size + 1)) - 0.5) * 0.25
 
@@ -80,3 +81,56 @@ regularization = (float(learning_rate) / (2 * m)) * (np.sum(np.power(theta1[:, 1
 J = J + regularization
 
 print(J)
+
+
+def sigmoid_grad(z):
+    return np.multiply(sigmoid(z), (1 - sigmoid(z)))
+
+
+def backprop(params, input_size, hidden_layer_size, num_labels, X, y, learning_rate):
+
+    m = X.shape[0]
+    X = np.matrix(X)
+    y = np.matrix(y)
+
+    theta1 = np.matrix(np.reshape(params[:hidden_layer_size * (input_size + 1)], (hidden_layer_size, (input_size + 1))))
+    theta2 = np.matrix(np.reshape(params[hidden_layer_size * (input_size + 1):],
+                                  (num_labels, (hidden_layer_size + 1))))
+
+    a1, z2, a2, z3, h = forward_propagate(X, theta1, theta2)
+
+    J = 0
+    theta1_grad = np.zeros(theta1.shape)
+    theta2_grad = np.zeros(theta2.shape)
+    # loop in range of labels of output, J is bigger
+    for i in range(num_labels):
+        J += 1 / m * np.sum(np.multiply(-y[:, i], np.log(h[:, i])) - np.multiply((1 - y[:, i]), np.log(1 - h[:, i])))
+
+    regularization = (float(learning_rate)/(2*m)) * (np.sum(np.power(theta1[:, 1:], 2)) + np.sum(np.power(theta2[:, 1:], 2)))
+
+    J = J + regularization
+
+    for i in range(m):
+        z2_i = np.insert(z2[i, :],0 ,values=np.ones(1))
+
+        delta3 = h[i, :] - y[i, :]
+        delta2 = np.multiply((delta3 * theta2), sigmoid_grad(z2_i))
+        delta2 = delta2[:, 1:]
+
+        theta1_grad = theta1_grad + delta2.T * a1[i, :]
+        theta2_grad = theta2_grad + delta3.T * a2[i, :]
+
+    theta1_grad = theta1_grad/m
+    theta2_grad = theta2_grad/m
+
+    theta1_grad[:, 1:] = theta1_grad[:, 1:] + learning_rate / m * theta1[:, 1:]
+    theta2_grad[:, 1:] = theta2_grad[:, 1:] + learning_rate / m * theta2[:, 1:]
+
+    grad = np.concatenate((np.ravel(theta1_grad), np.ravel(theta2_grad))    )
+
+    return J , grad
+
+
+J, grad = backprop(params, input_size, hidden_layer_size, num_labels, X, y_processed, learning_rate)
+print(J)
+print(grad.shape)
